994ded7b9d7e12f5e65f89d37ed91d2c91bff9a5
Christian <christian@korneck.de>
2015-09-14 16:29:52 +0900

    initial commit

---
+++ add-certs.cmd
@@ -0,0 +1,78 @@
+@echo off
+setlocal
+
+REM #### general config
+if not defined programfiles(x86) set programfiles(x86)=%programfiles%
+set firefoxdefaultprofile=%programfiles(x86)%\Mozilla Firefox\browser\defaults\Profile
+
+
+REM #### default firefox profile
+if not exist "%programfiles(x86)%\Mozilla Firefox" exit /B 1
+if not exist "%firefoxdefaultprofile%" mkdir "%firefoxdefaultprofile%"
+if not exist "%firefoxdefaultprofile%\cert8.db" copy /y "%~dp0db\empty\cert8.db" "%firefoxdefaultprofile%\" >NUL
+if not exist "%firefoxdefaultprofile%\key3.db" copy /y "%~dp0db\empty\key3.db" "%firefoxdefaultprofile%\" >NUL
+if not exist "%firefoxdefaultprofile%\secmod.db" copy /y "%~dp0db\empty\secmod.db" "%firefoxdefaultprofile%\" >NUL
+
+setlocal ENABLEDELAYEDEXPANSION
+set replacepath=%~dp0cacert\
+FOR /R "%~dp0" %%C IN (cacert\*.pem) DO (
+set certpath=%%C
+set certfile=!certpath:%replacepath%=!
+set certfile=!certfile:.pem=!
+set certfile=!certfile:.cacert=!
+set certfile=AddedByUser !certfile!
+"%~dp0bin\certutil.exe" -A -n "!certfile!" -i "%%C" -t "cTC,cTC,cTC", -d "%firefoxdefaultprofile%"
+)
+setlocal DISABLEDELAYEDEXPANSION
+
+
+REM ####user profiles
+setlocal ENABLEDELAYEDEXPANSION
+set replacepath=%~dp0cacert\
+FOR /D %%U IN ("%systemdrive%\Users\*") DO (
+FOR /D %%P IN ("%%U\AppData\Roaming\Mozilla\Firefox\Profiles\*") DO if not exist "%%P\cert8.db" copy /y "%~dp0db\empty\cert8.db" "%%P\" >NUL
+FOR /D %%P IN ("%%U\AppData\Roaming\Mozilla\Firefox\Profiles\*") DO if not exist "%%P\key3.db" copy /y "%~dp0db\empty\key3.db" "%%P\" >NUL
+FOR /D %%P IN ("%%U\AppData\Roaming\Mozilla\Firefox\Profiles\*") DO if not exist "%%P\secmod.db" copy /y "%~dp0db\empty\secmod.db" "%%P\" >NUL
+FOR /R "%~dp0" %%C IN (cacert\*.pem) DO (
+set certpath=%%C
+set certfile=!certpath:%replacepath%=!
+set certfile=!certfile:.pem=!
+set certfile=!certfile:.cacert=!
+set certfile=AddedByUser !certfile!
+FOR /D %%P IN ("%%U\AppData\Roaming\Mozilla\Firefox\Profiles\*") DO "%~dp0bin\certutil.exe" -A -n "!certfile!" -i "%%C" -t "cTC,cTC,cTC", -d "%%P"
+))
+setlocal DISABLEDELAYEDEXPANSION
+
+setlocal ENABLEDELAYEDEXPANSION
+set replacepath=%~dp0cacert\
+FOR /R "%~dp0" %%C IN (cacert\*.pem) DO (
+set certpath=%%C
+set certfile=!certpath:%replacepath%=!
+set certfile=!certfile:.pem=!
+set certfile=!certfile:.cacert=!
+set certfile=AddedByUser !certfile!
+FOR /D %%P IN ("%appdata%\Mozilla\Firefox\Profiles\*") DO (
+"%~dp0bin\certutil.exe" -A -n "!certfile!" -i "%%C" -t "cTC,cTC,cTC", -d "%%P"
+))
+setlocal DISABLEDELAYEDEXPANSION
+
+
+REM #### postcheck (check only on default profile, not individual user profiles)
+setlocal ENABLEDELAYEDEXPANSION
+set replacepath=%~dp0cacert\
+FOR /R "%~dp0" %%C IN (cacert\*.pem) DO (
+set certpath=%%C
+set certfile=!certpath:%replacepath%=!
+set certfile=!certfile:.pem=!
+set certfile=!certfile:.cacert=!
+set certfile=AddedByUser !certfile!
+"%~dp0bin\certutil.exe" -L -d "%firefoxdefaultprofile%" | findstr /i "!certfile!" >NUL
+set myerrorlevel=%errorlevel%
+if /i "!myerrorlevel!" NEQ "0" echo [ERR2] post-check: certificate not in db "!firefoxdefaultprofile!": !certfile!
+if /i "!myerrorlevel!" NEQ "0" exit /B 2
+)
+setlocal DISABLEDELAYEDEXPANSION
+
+
+REM #### eof
+exit /B 0
