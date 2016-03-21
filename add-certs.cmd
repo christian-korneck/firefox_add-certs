5b3c2e634d538f9652834aca77a6b0d0f045ff89
Slawomir Brzezinski <slawomirb@interxion.com>
2016-03-21 15:13:13 +0000

    Added crude -verbose flag for troubleshooting and some whitespace/comments cosmetics

--- add-certs.cmd
+++ add-certs.cmd
@@ -1,4 +1,4 @@
-@echo off
+@if /i "%1" NEQ "-verbose" @echo off
 setlocal
 
 REM #### general config
@@ -39,9 +39,13 @@ set certfile=!certpath:%replacepath%=!
 set certfile=!certfile:.pem=!
 set certfile=!certfile:.cacert=!
 set certfile=AddedByUser !certfile!
-FOR /D %%P IN ("%%U\AppData\Roaming\Mozilla\Firefox\Profiles\*") DO "%~dp0bin\certutil.exe" -A -n "!certfile!" -i "%%C" -t "cTC,cTC,cTC", -d "%%P"
+FOR /D %%P IN ("%%U\AppData\Roaming\Mozilla\Firefox\Profiles\*") DO (
+ "%~dp0bin\certutil.exe" -A -n "!certfile!" -i "%%C" -t "cTC,cTC,cTC", -d "%%P"
+)
 ))
 setlocal DISABLEDELAYEDEXPANSION
+
+REM ####Current user
 
 setlocal ENABLEDELAYEDEXPANSION
 set replacepath=%~dp0cacert\
